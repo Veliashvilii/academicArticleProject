df0f526d120fc92569cf2e8ea2dcb054e7409a46
Metehan Belli <metehanbelli8@gmail.com>
2024-05-01 13:31:17 +0300

    User can choose interest areas.

--- index/views.py
+++ index/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return render(request, 'index/index.html')
+  if request.method == 'GET':
+    interest_areas = [
+    "Artificial Intelligence and Machine Learning",
+    "Data Science and Big Data Analytics",
+    "Cybersecurity",
+    "Blockchain and Cryptocurrency",
+    "Computer Vision and Image Processing",
+    "Parallel and Distributed Computing",
+    "Computer Networks and Communications",
+    "Computer Graphics and Virtual Reality",
+    "Distributed Systems and Cloud Computing",
+    "Automated Programming and Code Generation"
+  ]
+    return render(request, 'index/index.html', {'interest_areas': interest_areas})
+  elif request.method == 'POST':
+    """
+    Request in post olduğu durumda yapılacak işlemler
+    """
+    pass
